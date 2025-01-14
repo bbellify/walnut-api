eef744f92bbb05a5b5116acf5abfa745950a6b6e
bbellify <brianpatrickbellinger@gmail.com>
2025-01-13 21:37:35 -0800

    rest of updates hooked up in zmq subscription

--- src/services/zeromq/index.ts
+++ src/services/zeromq/index.ts
@@ -1,13 +1,13 @@
 import { Subscriber } from 'zeromq';
 import SSE from '../sse';
 import {
-  getSummaryData
-  // getFeeData,
-  // getMiningData,
-  // getMempoolData,
-  // getDifficultyData,
-  // getNextBlockData,
-  // getLatestBlocksData
+  getSummaryData,
+  getFeeData,
+  getMiningData,
+  getMempoolData,
+  getDifficultyData,
+  getNextBlockData,
+  getLatestBlocksData
 } from '../../routes/dashboard/data';
 
 export default class BlockSubscriber {
@@ -40,11 +40,6 @@ export default class BlockSubscriber {
     console.log('New Block Hash Received:', blockHash);
 
     // summary
-    // fees
-    // mining
-    // mempool
-    // difficulty
-    // next block
     const summaryData = await getSummaryData();
     SSE.sendUpdate({
       update: {
@@ -53,12 +48,59 @@ export default class BlockSubscriber {
         timestamp: new Date().toISOString()
       }
     });
-
-    // const feeData = getFeeData();
-    // const miningData = getMiningData();
-    // const mempoolData = getMempoolData();
-    // const difficultyData = getDifficultyData();
-    // const nextBlockData = getNextBlockData();
-    // const latestBlocksData = getLatestBlocksData();
+    // fees
+    const feeData = await getFeeData();
+    SSE.sendUpdate({
+      update: {
+        type: 'fees',
+        data: feeData,
+        timestamp: new Date().toISOString()
+      }
+    });
+    // mining
+    const miningData = await getMiningData();
+    SSE.sendUpdate({
+      update: {
+        type: 'mining',
+        data: miningData,
+        timestamp: new Date().toISOString()
+      }
+    });
+    // mempool
+    const mempoolData = await getMempoolData();
+    SSE.sendUpdate({
+      update: {
+        type: 'mempool',
+        data: mempoolData,
+        timestamp: new Date().toISOString()
+      }
+    });
+    // difficulty
+    const difficultyData = await getDifficultyData();
+    SSE.sendUpdate({
+      update: {
+        type: 'difficulty',
+        data: difficultyData,
+        timestamp: new Date().toISOString()
+      }
+    });
+    // next block
+    const nextBlockData = await getNextBlockData();
+    SSE.sendUpdate({
+      update: {
+        type: 'nextblock',
+        data: nextBlockData,
+        timestamp: new Date().toISOString()
+      }
+    });
+    // latest blocks
+    const latestBlocksData = getLatestBlocksData();
+    SSE.sendUpdate({
+      update: {
+        type: 'latestblocks',
+        data: latestBlocksData,
+        timestamp: new Date().toISOString()
+      }
+    });
   }
 }
